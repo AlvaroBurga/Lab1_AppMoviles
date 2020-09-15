1a3b24d37564753c696a06d4f03c313e9de0a5bf
Alvaro Burga <a20150632@pucp.pe>
2020-09-14 22:28:16 -0500

    hola

---
+++ src/lab1appocalipsis/Plantilla.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Plantilla {
+    
+    void template()
+    {
+        Scanner sc= new Scanner(System.in);
+       boolean end = false;
+       while(true)
+       {
+            String a=sc.nextLine();
+            System.out.println(a);
+            switch(a)
+            {
+                case "1":
+                {
+                    break;
+                }
+                case "2":
+                {
+                    break;
+                }
+                case "3":
+                {
+                    break;
+
+                }
+                case "4":
+                {
+                    break;
+                }
+                case "5":
+                {
+                    break;
+                }
+                default:
+                {
+                    end=true;
+                }
+             }
+    
+    }
+    }
+      
+}
